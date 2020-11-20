2a8a3f735b92bfd4a5a64a4193fe483286c0b468
ShavdaAdhikari <ruksha_jisma@yahoo.com>
2020-11-20 09:46:14 -0500

    try new repository

---
+++ IdeaProjects/assignments/src/assignment1/Sumassignment.java
@@ -0,0 +1,26 @@
+package assignment1;
+
+public class Sumassignment {
+    public static void main(String[] args) {
+
+
+    int a=40;
+    int b=20;
+    int c= a+b;
+    int d= a-b;
+    int e= a*b;
+    int f=a/b;
+
+                System.out.println(" the first number is 40 and second no is 20 ");
+                System.out.println( " the sum of first and second is " +c);
+
+
+                System.out.println( " the difference of first and second is " +d);
+
+
+                System.out.println( " the product of first and second is " +e);
+
+
+                System.out.println( " the divide of first and second is " +f);
+}
+}
